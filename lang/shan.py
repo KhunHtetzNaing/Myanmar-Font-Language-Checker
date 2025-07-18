bc611ee7148c4d27c1e219e5f6cd8b896b12ba2a
Sai Mao (DELL) <saisainghminetun@gmail.com>
2025-07-18 11:15:49 +0800

    Added for Shan.

---
+++ lang/shan.py
@@ -0,0 +1,95 @@
+LANGUAGE_DATA = {
+    "name": "Shan",
+    "characters": [
+        # 19 Major Consonants
+        0x1075,  # ၵ
+        0x1076,  # ၶ
+        0x1004,  # င
+        0x1078,  # ၸ
+        0x101E,  # သ
+        0x107A,  # ၺ
+        0x1010,  # တ
+        0x1011,  # ထ
+        0x107C,  # ၼ
+        0x1015,  # ပ
+        0x107D,  # ၽ
+        0x107E,  # ၾ
+        0x1019,  # မ
+        0x101A,  # ယ
+        0x101B,  # ရ
+        0x101C,  # လ
+        0x101D,  # ဝ
+        0x1081,  # ႁ
+        0x1022,  # ဢ
+
+        # Vowel Signs (Expect Consonant Combination)
+        0x1062, # ၢ
+        0x102D, # ိ
+        0x1035, # ဵ
+        0x1085, # ႅ
+
+        # Vowel Signs (Used without Consonant Combination)
+        0x1083, # ႃ
+        0x102E, # ီ
+        0x1084, # ႄ
+        0x109B, # ႆ
+
+        # Vowel Signs (Can or cannot have consonant combinations)
+        0x102F, # ု
+        0x1030, # ူ
+        0x1031, # ေ
+
+        # 5 tone mark
+        0x1087,  # ႇ
+        0x1088,  # ႈ
+        0x1038,  # း
+        0x1089,  # ႉ
+        0x108A,  # ႊ
+
+        # asat
+        0x103a,  # ်
+
+        # Independent Vowels
+        0x109E, # ႞
+        0x109F, # ႟
+
+        # Median
+        0x103b,  # ျ
+        0x103c,  # ြ
+        0x103d,  # ွ
+        0x1082,  # ႂ
+
+        # Other Consonant mainly used for pali
+        0x1077,  # ၷ
+        0xA9E0, # ꧠ
+        0xAA61, # ꩡ
+        0xA9E1, # ꧡ
+        0xA9E2, # ꧢ
+        0x107B, # ၻ
+        0xAA66, # ꩦ
+        0xAA68, # ꩨ
+        0xAA6A, # ꩪ
+        0xAA67, # ꩧ
+        0xAA69, # ꩩ
+        0xA9E3, # ꧣ
+        0x107F, # ၿ
+        0xA9E4, # ꧤ
+        0xAA6E, # ꩮ
+
+        # Digits
+        0x1090,  # ႐
+        0x1091,  # ႑
+        0x1092,  # ႒
+        0x1093,  # ႓
+        0x1094,  # ႔
+        0x1095,  # ႕
+        0x1096,  # ႖
+        0x1097,  # ႗
+        0x1098,  # ႘
+        0x1099,  # ႙
+
+        # Punctuation
+        0x104a,  # ၊
+        0x104b,  # ။
+    ]
+}
